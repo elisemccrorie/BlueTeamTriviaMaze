7b0e47514ee164f6c3dc239172d46e53dd0c1730
Cord Rehn <jordansg57@gmail.com>
2014-05-13 14:26:48 -0700

    Added Player.cs & Maze.cs
    Initial (skeleton) commit of the Player and Maze classes with summaries.

---
+++ BlueTeamTriviaMaze/BlueTeamTriviaMaze/Maze.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueTeamTriviaMaze
+{
+    /// <summary>
+    /// Maze - the container for a Player to navigate through a collection 
+    ///        of Rooms, each composed of doors that will only open if a
+    ///        question is answered correctly.
+    /// </summary>
+    
+
+
+    class Maze
+    {
+    }
+}
---
+++ BlueTeamTriviaMaze/BlueTeamTriviaMaze/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueTeamTriviaMaze
+{
+    /// <summary>
+    /// Player - the entity that moves between Rooms through Doors
+    ///          in the Maze in the 4 cardinal directions (N,S,E,W).
+    /// </summary>
+
+
+
+    class Player
+    {
+    }
+}

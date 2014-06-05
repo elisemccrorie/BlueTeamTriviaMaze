b0e58d359893686cb91cdb7f1d1e85053ef3c642
e-peterson <e.peterson2014@hotmail.com>
2014-06-04 22:25:17 -0700

    Adding icon

--- BlueTeamTriviaMaze/BlueTeamTriviaMaze/Properties/Resources.Designer.cs
+++ BlueTeamTriviaMaze/BlueTeamTriviaMaze/Properties/Resources.Designer.cs
@@ -663,6 +663,26 @@ namespace BlueTeamTriviaMaze.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap TriviaMazeIcon {
+            get {
+                object obj = ResourceManager.GetObject("TriviaMazeIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon TriviaMazeIcon1 {
+            get {
+                object obj = ResourceManager.GetObject("TriviaMazeIcon1", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Width {
             get {
                 object obj = ResourceManager.GetObject("Width", resourceCulture);

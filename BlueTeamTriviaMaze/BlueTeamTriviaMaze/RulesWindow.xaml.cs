58cc168c542c158e53a5c1cd60a654fba88e2660
e-peterson <e.peterson2014@hotmail.com>
2014-06-11 16:51:33 -0700

    Updated Rules and docs
    Updated the rules in the game
    added the most recent versions of coding standards and srs
    added the installer

--- BlueTeamTriviaMaze/BlueTeamTriviaMaze/RulesWindow.xaml.cs
+++ BlueTeamTriviaMaze/BlueTeamTriviaMaze/RulesWindow.xaml.cs
@@ -15,9 +15,11 @@ namespace BlueTeamTriviaMaze
         public RulesWindow()
         {
             InitializeComponent();
+
             txtblkRules.Text = String.Format("Navigate through the maze by clicking a door in the same room as your character." +
-                "A question will pop up, if answered correctly your character will move through the selected door. If answered"+
-                " incorrectly the selected door will be locked for the duration of the game. The game is lost if you become"+
+                "A question will pop up, if answered correctly your character will move through the selected door. If answered" +
+                " incorrectly the selected door will be locked. Locked doors can only be opened with keys." +
+                " The player is given three keys at the begining of the game. The game is lost if you become" +
                 " locked in a room or are locked out of the exit room. You win if you reach the exit.");
         }
     }
